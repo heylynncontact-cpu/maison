8a840bb7f12c3d0f8f230ef1adfedb1db3aa18b4
heylynncontact-cpu <heylynn.contact@gmail.com>
2026-07-14 09:52:56 +0200

    Delete notre-maison directory

--- notre-maison/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/maison/', // nom du dépôt GitHub Pages — https://heylynncontact-cpu.github.io/maison/
-})
